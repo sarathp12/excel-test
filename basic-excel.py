1a6f4ecffb55dcee457a77e7e0afc3b454d867ce
sarathy <sarathy@example.com>
2019-03-23 17:33:18 +0000

    print two columsn one below the other

--- basic-excel.py
+++ basic-excel.py
@@ -8,7 +8,8 @@ worksheet = workbook.sheet_by_index(0)
 print("the value at row 8 and column 6 is : {0}".format(worksheet.cell(8,6).value))
 
 # extract the whole column
-
+# print the two columns line by line
 worksheet.cell_value(0, 0)
 for i in range(worksheet.nrows):
+    print(worksheet.cell_value(i, 5))
     print(worksheet.cell_value(i, 6))
