cce64be55151c586da03aef6946043ed4350dbee
sarath <sarathy@example.com>
2019-03-25 02:32:48 +0000

    print the two columns by ips and ports

--- pandas-excel-work.py
+++ pandas-excel-work.py
@@ -9,3 +9,13 @@ ip_data = pd.read_excel(excel_file)
 x = ip_data.head()
 print(x)
 
+# print the excel sheets
+ip_data_sheet1 = pd.read_excel(excel_file, sheetname=0, index_col=0)
+y = ip_data_sheet1.head()
+print(y)
+
+# dynamically choose the columns
+df = pd.read_excel('test-py.xlsx')
+df1 = df[df.columns[5:7]].dropna()
+df1.columns = ['ip','ports']
+print(df1)
